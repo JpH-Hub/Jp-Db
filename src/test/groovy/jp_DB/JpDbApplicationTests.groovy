f1e0ab1bfaca6fef19f51333f20e2ca4ad81dc82
João Pedro Melo <jpmelodasilva309@gmail.com>
2024-10-25 10:39:42 -0300

    Initial commit

---
+++ src/test/groovy/jp_DB/JpDbApplicationTests.groovy
@@ -0,0 +1,13 @@
+package jp_DB
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JpDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
